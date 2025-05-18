ec7a1411d16e1dac573a713ea513fa14cf1ea973
whosam92 <banialihusm@gmail.com>
2025-05-18 17:17:55 +0300

    faq edit

--- resources/views/front/faq.blade.php
+++ resources/views/front/faq.blade.php
@@ -20,60 +20,49 @@
                             <div class="accordion-item">
                                 <h2 class="accordion-header" id="headingOne">
                                 <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                    Accordion Item #1
+ How can I modify or cancel my reservation?
                                 </button>
                                 </h2>
                                 <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                                     <div class="accordion-body">
                                         <p>
-                                            Lorem ipsum dolor sit amet, ut has quidam prodesset, eos sumo ipsum civibus ea, vel quas nusquam ei. Et sea doming quodsi audire. No vim ornatus scaevola disputando, qui stet ceteros ad. Ad his choro appetere mnesarchum, no duo accusata incorrupte, vel essent fabulas ut.
-                                        </p>
+You can modify or cancel your reservation by visiting the "Contact Us" page on our website and send us a message or find the phone number up in the Navbar in our website so our team will help you .                                      </p>
                                         <p>
-                                            Ne nam soluta libris. Cu sea utamur adipiscing, convenire patrioque dignissim et nec. Accusam incorrupte vituperatoribus vix ad, ei clita omnium mentitum pro. Est ad duis perpetua recteque, in autem posidonium qui. Illum nulla dolor mea an.
-                                        </p>
-                                        <p>
-                                            Officiis disputationi ne pri, libris malorum eam id. Molestie principes vix no. Ut velit iudicabit inciderint mea. Malorum mediocrem deseruisse nam ne, tale imperdiet vim ut. Aperiri splendide cu eos, vis in alia laoreet aliquando.
-                                        </p>
+Make sure you have your booking reference number on the invoices that you can share with our team and email it on info@JoHotel.com to change, or cancel your reservation as per our cancellation policy.                                          </p>
+                                       
                                     </div>
                                 </div>
                             </div>
                             <div class="accordion-item">
                                 <h2 class="accordion-header" id="headingTwo">
                                 <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                    Accordion Item #2
+                                    Is my credit card charged at the time of booking?
                                 </button>
                                 </h2>
                                 <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
                                     <div class="accordion-body">
                                         <p>
-                                            Omnis aliquip mel ne. Diam tantas mea ex, ad magna luptatum qui, cum ea veniam persecuti. Id suavitate prodesset vis. Intellegat intellegam ad mei, nec oblique petentium definitiones in. Vim lorem numquam tibique cu, rebum iriure mediocrem te nam, semper mandamus conceptam an usu.
+                                              Yes, if you enter your Card Details at the checkout page and choose to complete  booking a room select, our website will process your request and go with immediate payment to your card. 
                                         </p>
                                         <p>
-                                            No unum feugait est, ei ius quem adhuc definitionem. Odio porro decore cu sit, eum adhuc semper dolores id, eam te nostro legimus. Ne utroque patrioque per, in consul nominavi has. Choro patrioque eum ex, erant tacimates persecuti sit an. No nec dicant delenit, vocent regione eam te.
-                                        </p>
+Please check the payment terms during the booking process for specific details before you submit your request.                                        </p>
                                     </div>
                                 </div>
                             </div>
                             <div class="accordion-item">
                                 <h2 class="accordion-header" id="headingThree">
                                 <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                    Accordion Item #3
+                                    Are taxes and fees included in the room rate?
                                 </button>
                                 </h2>
                                 <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
                                     <div class="accordion-body">
                                         <p>
-                                            Aliquid sententiae theophrastus qui eu. Vix ad lorem pertinax. Cu labore lucilius recusabo pro, in per temporibus reformidans. Ex scripta fastidii expetendis ius, iudico nusquam et mel.
-                                        </p>
+total amount displayed during booking includes applicable taxes and fees.                                       </p>
                                         <p>
-                                            Et vide legimus maiorum eos, ius an delenit copiosae mandamus. Ex sale intellegebat qui, an admodum theophrastus mei. Vix ne eros ludus legere, sed cu suas torquatos. Feugait consequat nam no, nec causae ullamcorper ex. Mazim saepe vix et.
+                                             Additional charges, such as resort fees or city taxes, may apply and will be clearly stated during the booking process. 
                                         </p>
-                                        <p>
-                                            Eos erat utinam ei, cum saepe everti deleniti et, alterum apeirian qualisque nam ex. Vim id omnesque intellegebat, ut eum solum eruditi, ut viderer recusabo argumentum est. Nam ex elit dolores percipit. Sea quodsi propriae epicurei at, an simul expetendis duo, an vidisse detracto officiis sit. Eu aperiri iuvaret vel.
-                                        </p>
-                                        <p>
-                                            Id offendit euripidis efficiantur vim, clita virtute eloquentiam ea eum. Te usu iracundia constituam repudiandae, cu nam laudem delicata intellegam. Dico theophrastus sed ex. Ex vis zril nominati recteque, tollit vituperatoribus an sit, epicurei temporibus concludaturque et mei.
-                                        </p>
+                                        
                                     </div>
                                 </div>
                             </div>
